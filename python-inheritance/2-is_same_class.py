abd59a8c9c0ff8477d578702550a2c0a35e1eccd
FontGuillaume <guillaume_font@orange.fr>
2025-05-26 15:33:21 +0200

    Amélioration de la documentation du module is_same_class pour une meilleure lisibilité.

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-Module qui définit la fonction is_same_class pour vérifier le type exact d'un objet.
+Module qui définit la fonction is_same_class
+pour vérifier le type exact d'un objet.
 """
 
 
 def is_same_class(obj, a_class):
     """
     Vérifie si un objet est exactement une instance de la classe spécifiée.
-    
+
     Args:
         obj: L'objet à vérifier
         a_class: La classe à comparer
-    
+
     Returns:
-        True si l'objet est exactement une instance de la classe spécifiée, sinon False
+        True si l'objet est exactement une
+        instance de la classe spécifiée, sinon False
     """
     return type(obj) is a_class
