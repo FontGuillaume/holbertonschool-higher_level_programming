afb4205059406c31e79ffd200d09d0a650b5082c
FontGuillaume <guillaume_font@orange.fr>
2025-05-26 15:35:02 +0200

    Amélioration de la documentation de la classe BaseGeometry et ajout de la méthode integer_validator pour valider les entiers positifs.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,8 +9,7 @@ class BaseGeometry:
     """
     Classe de base pour les formes géométriques.
 
-    Cette classe est conçue pour être une classe de
-    base pour différentes
+    Cette classe est conçue pour être une classe de base pour différentes
     formes géométriques et sera étendue par des classes filles.
     """
 
@@ -25,3 +24,20 @@ class BaseGeometry:
             implémentée dans la classe de base
         """
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Valide que la valeur est un entier positif.
+
+        Args:
+            name (str): Le nom de la valeur à valider
+            value: La valeur à valider
+
+        Raises:
+            TypeError: Si value n'est pas un entier
+            ValueError: Si value est inférieur ou égal à 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
