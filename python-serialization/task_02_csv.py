6e516d47bf2d4e1ac19acadf3e855aa8e9c731b3
FontGuillaume <guillaume_font@orange.fr>
2025-06-03 14:26:43 +0200

    Correction de l'ouverture du fichier JSON et amélioration de la gestion des exceptions dans la fonction convert_csv_to_json.

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -18,11 +18,13 @@ def convert_csv_to_json(filename):
     """
     try:
         with open(filename, "r", encoding="utf-8") as csvfile:
-            READER = csv.DictReader(csvfile)
-        data = list(csv.DictReader(csvfile))
-        with open(data.json, "w", encoding="utf-8") as jsonfile:
+            data = list(csv.DictReader(csvfile))
+
+        with open('data.json', "w", encoding="utf-8") as jsonfile:
             json.dump(data, jsonfile)
         return True
 
     except FileNotFoundError:
+        return False
+    except Exception:
         return False
