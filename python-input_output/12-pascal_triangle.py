c13e0b87a1898806466e7e321582a8373f8e2540
FontGuillaume <guillaume_font@orange.fr>
2025-06-05 12:20:56 +0200

    Refactor la fonction pascal_triangle pour simplifier la génération du triangle de Pascal et améliorer la lisibilité du code.

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -17,19 +17,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
-
-    for i in range(n):
+    result = [[1]]
+    
+    for i in range(1, n):
         row = [1]
-
-        if i > 1:
-            for j in range(1, i):
-                val = triangle[i - 1][j - 1] + triangle[i - 1][j]
-                row.append(val)
-
-        if i > 0:
-            row.append(1)
-
-        triangle.append(row)
-
-    return triangle
+        prev_row = result[i - 1]
+        
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+            
+        row.append(1)
+        result.append(row)
+        
+    return result
