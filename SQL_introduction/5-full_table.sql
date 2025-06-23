ceadb8f64492a6254411e45b1e6918a054008955
FontGuillaume <guillaume_font@orange.fr>
2025-06-23 15:19:49 +0200

    Correction des requêtes SQL : ajout de la table dans la commande SHOW CREATE TABLE et précision de la condition dans la requête COUNT

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- La commande SHOW CREATE TABLE renvoie la requête SQL exacte qui a été utilisée pour créer la table
 -- Cette commande est utile pour examiner en détail la structure d'une table, y compris ses contraintes,
 -- ses types de données, et autres propriétés définies lors de sa création
-SHOW CREATE table
+SHOW create table first_table;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -2,4 +2,4 @@
 -- La fonction COUNT(*) renvoie le nombre total de lignes qui correspondent à la condition
 -- La clause WHERE filtre les résultats pour ne compter que les lignes où id=89
 -- Cette requête est utile pour vérifier rapidement combien d'entrées correspondent à un critère spécifique
-SELECT COUNT(*) FROM first_table WHERE id =
+SELECT COUNT(*) FROM first_table WHERE id = 89;
