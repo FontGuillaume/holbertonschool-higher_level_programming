5e844a43589d8bb24d0a7721f144d78628df6cb4
FontGuillaume <guillaume_font@orange.fr>
2025-06-25 09:40:53 +0200

    Correction de la requête SQL pour lister les villes de Californie : sélection des colonnes id et name au lieu de toutes les colonnes

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,7 @@
 -- Il utilise une sous-requête pour récupérer l'ID de l'état de Californie
 -- Les résultats sont triés par ID de ville en ordre croissant
 -- La sous-requête évite d'avoir à connaître l'ID exact de la Californie
-SELECT * FROM cities
+SELECT id, name
+FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
