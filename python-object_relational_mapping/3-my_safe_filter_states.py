2e567bfb21243be12afb5da7e78efdf2ac63b991
FontGuillaume <guillaume_font@orange.fr>
2025-06-27 17:52:23 +0200

    Ajout de scripts Python pour la sélection et le filtrage des états dans la base de données, avec gestion des connexions et des requêtes SQL.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+    name_search = sys.argv[4]
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (name_search,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
