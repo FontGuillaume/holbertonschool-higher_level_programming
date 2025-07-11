056e24e7d79495bc7bd4000864dea60b4b38acc2
FontGuillaume <guillaume_font@orange.fr>
2025-07-11 15:24:39 +0200

    Ajout de la gestion des cas NaN dans la fonction d'addition pour améliorer la robustesse du code.

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-function add (a, b) {
+function add(a, b) {
   return a + b;
 }
 const argv = process.argv;
 const a = parseInt(argv[2]);
 const b = parseInt(argv[3]);
-console.log(add(a, b));
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(add(a, b));
+}
