bf6e5b98b5dc62f4074516275f2bdb280fdc9f8f
FontGuillaume <guillaume_font@orange.fr>
2025-07-11 11:18:35 +0200

    Amélioration de la lisibilité du fichier 3-value_argument.js en séparant les instructions console.log et ajout du fichier 4-concat.js.

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
-console.log(argv[2] !== undefined ? argv[2] : 'No argument');
+if (argv[2] !== undefined) {
+  console.log(argv[2]);
+} else {
+  console.log('No argument');
+}
