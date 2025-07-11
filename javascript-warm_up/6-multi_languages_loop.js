03be2d3bc55cbc472270cdcd9c0ff1f9ba9e049b
FontGuillaume <guillaume_font@orange.fr>
2025-07-11 15:21:57 +0200

    Suppression de la variable de sortie et affichage direct des lignes dans 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let output = '';
 for (let i = 0; i < lines.length; i++) {
-  output += lines[i] + '\n';
+  console.log(lines[i]);
 }
-console.log(output.slice(0, -1));
